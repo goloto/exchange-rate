9e0916f15f715fe5f61ca3f4dc823a1a8aaca5ce
Mikhail Goloto <goloto@pm.me>
2020-10-15 16:32:56 +0300

    create exchange-visitor component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ExchangeRateComponent } from './exchange-rate/exchange-rate.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExchangeRateComponent
   ],
   imports: [
     BrowserModule
